d1b0decba9eacf88a1166c274312635397b26a0a
Guy Morita <guymorita@gmail.com>
2017-05-08 14:33:28 -0700

    add components to index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
 export { default as GComponent } from "./components/g-component";
+export { default as RenderFriend } from "./components/render-friend";
+export { default as GuestList } from "./components/guest-list";
